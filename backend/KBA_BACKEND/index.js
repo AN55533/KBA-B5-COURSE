34f7ddb7c642e193a68d880eeb10e88ef0836e34
AN55533 <anjujv.33@gmail.com>
2025-07-28 12:13:48 +0530

    commit

---
+++ backend/KBA_BACKEND/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening to port ${process.env.PORT} `)
+});
